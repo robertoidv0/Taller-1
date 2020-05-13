77003d75e72d34766ce282675357e85abeef35b3
robertoidv0 <64909007+robertoidv0@users.noreply.github.com>
2020-05-13 12:42:59 -0400

    Update factorialyarreglo.cpp
    comparación resultado iterativo y recursivo

--- factorialyarreglo.cpp
+++ factorialyarreglo.cpp
@@ -3,24 +3,24 @@
 
 #define tam 500
 
-/*int factorial(int n){
+int factorial_rec(int n){
 	if(n==1){
 		return(1);
 		}
 	else{
-		return(n*factorial(n-1));
+		return(n*factorial_rec(n-1));
 	}
-}*/
+}
 
-int imprimir(int resultado[]){
+int imprimir(int resultado[],int n){
 	printf("\n\nfuncion imprimir: \n\n el arreglo es: ");
-    for(int e=0;e<=tam-2; e++){
+    for(int e=0; e<=n-2; e++){
 		printf("[%d]->",resultado[e]);
 	}
 	printf("\n\n");
 }
 
-int factorial(int n,int resultado[tam]){
+int factorial_it(int n,int resultado[tam]){
 	int factorial=1;
 	int pos=0;
 	for(int i=2;i<=n;i++){
@@ -43,9 +43,11 @@ int main(){
 	scanf("%i",&n);
 //	int tam=n;
 	int resultado[tam];
-	int valor=factorial(n,resultado);
-    printf("\nel valor es: %d \n",valor);
+	int valor=factorial_it(n,resultado);
+	int valor2=factorial_rec(n);
+    printf("\nel valor iterativo es: %d \n",valor);
+    printf("\nel valor recursivo es: %d \n",valor2);
    // printf("\n arreglo es: %d-> %d-> %d-> %d  \n\n",resultado[0],resultado[1],resultado[2],resultado[3]);
-    //imprimir(resultado);
+    imprimir(resultado,n);
 	system("pause");
 }
